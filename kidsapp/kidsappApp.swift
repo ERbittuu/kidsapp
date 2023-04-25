bec9e0acd6de7b456c9d367221447946af3b132d
Utsav Patel <utsavhacker@gmail.com>
2023-04-25 13:10:05 +0400

    Initial Commit

---
+++ kidsapp/kidsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidsappApp.swift
+//  kidsapp
+//
+//  Created by Utsav Patel on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct kidsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
